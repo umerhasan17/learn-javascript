70a36d3a64fa36d4fe3ca9047a09dafca2840d4f
unveiledrain <umerhasan17@gmail.com>
2018-08-05 11:18:28 +0100

    Updating style.css to fit tutorial and fixing bugs in script.js to get functionality to the end of part 6.

--- Noughts&Crosses/script.js
+++ Noughts&Crosses/script.js
@@ -84,15 +84,14 @@ function bestSpot() {
 }
 
 function checkTie() {
-    if (emptySquares().length == 0) {
-        for (var i = 0 ; i , cells.length ; i++) {
-            cells[i].style.backgroundColor = "green";
-            cells[i].removeEventListener('click' , turnClick , false);
-        }
-        declareWinner("Tie Game.");
-        return true;
-    }
-    return false;
+	if (emptySquares().length == 0) {
+		for (var i = 0; i < cells.length; i++) {
+			cells[i].style.backgroundColor = "green";
+			cells[i].removeEventListener('click', turnClick, false);
+		}
+		declareWinner("Tie Game.")
+		return true;
+	}
+	return false;
 }
-
 
