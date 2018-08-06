b7419a40fbebec891e53b13944e1fcd903e31597
unveiledrain <umerhasan17@gmail.com>
2018-08-06 11:45:21 +0100

    Bug in grid.render()

--- gameoflife/src/index.js
+++ gameoflife/src/index.js
@@ -2,15 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class Box extends React.Component {
+    selectBox = () => {
+        this.props.selectBox(this.props.row , this.props.col)
+    }
+
+    render() {
+        return (
+            <div className={this.props.boxClass} id={this.props.id} onClick={this.selectBox}/>
+        )
+    }
+}
+
 class Grid extends React.Component {
     render() {
         const width = this.props.cols * 14;
         var rowsArr = [];
 
         var boxClass = "";
+
         for (var i = 0 ; i < this.props.rows ; i ++) {
             for (var j = 0 ; j < this.props.cols ; j++) {
                 let boxId = i + "_" + j;
+                
                 boxClass = this.props.gridFull[i][j] ? "box on" : "box off";
                 rowsArr.push(
                     <Box 
@@ -21,7 +35,7 @@ class Grid extends React.Component {
                         col={j} 
                         selectBox={this.props.selectBox} 
                     />
-                )
+                );
             }
         }
 
