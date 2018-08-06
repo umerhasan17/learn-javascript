3303bf555d7b825b7bcca66789b2ec1da120459a
unveiledrain <umerhasan17@gmail.com>
2018-08-06 11:36:04 +0100

    End of part 3 with  an intentional bug in the program. Box is not defined.

--- gameoflife/src/index.js
+++ gameoflife/src/index.js
@@ -4,8 +4,30 @@ import './index.css';
 
 class Grid extends React.Component {
     render() {
+        const width = this.props.cols * 14;
+        var rowsArr = [];
+
+        var boxClass = "";
+        for (var i = 0 ; i < this.props.rows ; i ++) {
+            for (var j = 0 ; j < this.props.cols ; j++) {
+                let boxId = i + "_" + j;
+                boxClass = this.props.gridFull[i][j] ? "box on" : "box off";
+                rowsArr.push(
+                    <Box 
+                        boxClass={boxClass} 
+                        key={boxId} 
+                        boxId={boxId} 
+                        row={i} 
+                        col={j} 
+                        selectBox={this.props.selectBox} 
+                    />
+                )
+            }
+        }
+
+
         return (
-            <div>Grid</div>
+            <div className="grid" style={{width: width}}>{rowsArr}</div>
         );
     }
 }
@@ -14,8 +36,13 @@ class Main extends React.Component {
 
     constructor() {
         super();
+        this.speed = 100;
+        this.rows = 30;
+        this.cols = 50;
         this.state = {
-            generation:0
+            generation: 0,
+            gridFull: Array(this.row).fill().map(() => Array(this.cols).fill(false)),
+
         }
     }
 
@@ -23,7 +50,12 @@ class Main extends React.Component {
         return (
             <div>
                 <h1>The Game of Life</h1>
-                <Grid />
+                <Grid 
+                    gridFull={this.state.gridFull} 
+                    rows={this.rows} 
+                    cols={this.cols} 
+                    selectBox={this.selectBox}
+                />
                 <h2>Generations: {this.state.generation}</h2>
             </div>
         );
