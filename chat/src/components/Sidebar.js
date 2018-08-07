2c74714c279121b1565bb927dad87032385465df
unveiledrain <umerhasan17@gmail.com>
2018-08-07 16:21:50 +0100

    Fixed few more typos. Chrome still showing 1 error on load and 4 error after typing in text.

--- chat/src/components/Sidebar.js
+++ chat/src/components/Sidebar.js
@@ -4,9 +4,9 @@ import PropTypes from 'prop-types';
 const Sidebar = ({users}) => (
     <aside id="sidebar" className="sidebar">
         <ul>
-            {users.map(user => {
+            {users.map(user => (
                 <li key={user.id}>{user.name}</li>
-            })}
+            ))}
         </ul>
     </aside>
 )
@@ -15,7 +15,7 @@ Sidebar.PropTypes = {
     users: PropTypes.arrayOf(
         PropTypes.shape({
             id: PropTypes.number.isRequired,
-            name: PropTypes.number.isRequired
+            name: PropTypes.string.isRequired
         }).isRequired
     ).isRequired
 }
