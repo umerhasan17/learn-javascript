ee48cd90a96ccfdd418b1e78355f518665795220
unveiledrain <umerhasan17@gmail.com>
2018-08-07 09:05:34 +0100

    Created the ActionTypes and Constants.

--- chat/src/index.js
+++ chat/src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux'; // where all the app data is stored
+import chat from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(chat);
+
+// initialise redux
+
+// import chat reducer - part of Redux
+
+// create the store
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
 registerServiceWorker();
