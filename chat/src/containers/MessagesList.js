034f7f73ba422669643c4615bc194ba3ce7bd98c
unveiledrain <umerhasan17@gmail.com>
2018-08-07 16:32:49 +0100

    Trying to find more bugs.

--- chat/src/containers/MessagesList.js
+++ chat/src/containers/MessagesList.js
@@ -1,7 +1,6 @@
-import {connect} from 'react-redux';
-import MessagesListComponent from '../components/MessagesList';
-import {addMessage} from '../actions'
+import {connect} from 'react-redux'
+import MessagesListComponent from '../components/MessagesList'
 
 export const MessagesList = connect(state => ({
     messages:state.messages
-}), {} ) (MessagesListComponent) 
+}), {})(MessagesListComponent) 
