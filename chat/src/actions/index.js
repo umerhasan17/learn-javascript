8cbc43bf77b2859d47ffb1ebbdcd745431d7d0d2
unveiledrain <umerhasan17@gmail.com>
2018-08-07 17:00:58 +0100

    Finally found bug. After copying and pasting the file. It was a bug in components/Message.js the message and author was not enclosed in a double bracket.

--- chat/src/actions/index.js
+++ chat/src/actions/index.js
@@ -1,4 +1,4 @@
-import * as types from '../constants/ActionTypes';
+import * as types from '../constants/ActionTypes'
 
 let nextMessageId = 0;
 let nextUserId = 0;
@@ -7,27 +7,27 @@ let nextUserId = 0;
 // reducers take care of creating a new state when the action is dispatched
 // reducers created in the reducers directory.
 
-export const addMessage = (message , author) => ({
-    type: types.ADD_MESSAGE,
-    id: nextMessageId++,
-    message,
-    author
+export const addMessage = (message, author) => ({
+	type: types.ADD_MESSAGE,
+	id: nextMessageId++,
+	message,
+	author
 })
 
 export const addUser = name => ({
-    type: types.ADD_USER , 
-    id: nextUserId++,
-    name
+	type: types.ADD_USER,
+	id: nextUserId++,
+	name
 })
 
-export const messageReceived = (message ,author) => ({
-    type: types.MESSAGE_RECEIVED,
-    id: nextMessageId++,
-    message,
-    author
+export const messageReceived = (message, author) => ({
+	type : types.MESSAGE_RECEIVED,
+	id: nextMessageId++,
+	message,
+	author
 })
 
 export const populateUsersList = users => ({
-    type: types.USERS_LIST,
-    users
+	type: types.USERS_LIST,
+	users
 })
