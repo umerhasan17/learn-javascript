6e26497f62691e934aaf6377bf7f20dd62789d43
unveiledrain <umerhasan17@gmail.com>
2018-08-09 08:43:39 +0100

    body parser section has bugs cannot recognise title and body

--- notable/app/routes/note_routes.js
+++ notable/app/routes/note_routes.js
@@ -1,6 +1,8 @@
 module.exports = function (app , db) {
     app.post('/notes' , (req , res) => {
         // We'll create the note here
+        // express can't understand the body part alone. body-parser can however. 
+        console.log(req.body);
         res.send('Hello');
     })
 }
--- notable/server.js
+++ notable/server.js
@@ -7,6 +7,8 @@ const app = express();
 
 const port = 8000;
 
+app.use(bodyParser.urlencoded({extended: true}))
+
 require('./app/routes')(app , {});
 
 // sets up the server
