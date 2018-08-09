0bde338ee3a932a6617f10f675dbf5fe0e0b4478
unveiledrain <umerhasan17@gmail.com>
2018-08-09 08:39:48 +0100

    First route operational with no bugs

---
+++ notable/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const MongoClient = require ('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+
+// initialise app as instance of express (the framework)
+const app = express();
+
+const port = 8000;
+
+require('./app/routes')(app , {});
+
+// sets up the server
+app.listen(port , () => {
+    console.log("We are live on " + port);
+})
+
+
