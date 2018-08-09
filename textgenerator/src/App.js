9fe64a0f75715671f0c5d5011c8d6a09d8e3a112
unveiledrain <umerhasan17@gmail.com>
2018-08-09 16:44:24 +0100

    No 'Access-Control-Allow-Origin' header is present on the requested resource.
    Error when doing the request to API

--- textgenerator/src/App.js
+++ textgenerator/src/App.js
@@ -20,14 +20,14 @@ class App extends Component {
 
   getSampleText() {
     axios.get('http://hipsterjesus.com/api?paras='+this.state.paras+'&html='+this.state.html)
-      .then((response => {
+      .then((response) => {
         this.setState({text : response.data.text} , function () {
           console.log(this.state);
         });
-      }))
+      })
       .catch((err) => {
         console.log(err);
-      })
+      });
   }
   
   render() {
