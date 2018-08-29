7557838849021a71e9621ab7f6ee9e0229bca84f
unveiledrain <umerhasan17@gmail.com>
2018-08-29 22:21:24 +0100

    Run server. Performing migrations. Adding url patterns and installed apps.

--- just django fullstack tutorial/backend/src/djreact/urls.py
+++ just django fullstack tutorial/backend/src/djreact/urls.py
@@ -17,5 +17,7 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    path('api-auth/' , include('rest_framework.urls')),
     path('admin/', admin.site.urls),
+    
 ]
